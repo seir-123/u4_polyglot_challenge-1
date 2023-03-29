92e750d321b1d07cda976a983a2f3c27b6388899
Austin <austinih46@gmail.com>
2023-03-28 22:58:38 -0500

    completed Polyglot

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 # - If called with no arguments, return 0 (zero).
 # - If any non-number arguments are in the argument, return "NaN"
 
-
 # Examples:
 # add(1) //=> 1
 # add(1,50,1.23) //=> 52.23
@@ -14,10 +13,20 @@
 
 #-----------------------------------------------
 # Solution Goes Here - >
+def add_list(*numbers):
+    total = 0
+    for num in numbers:
+        if isinstance(num, (int, float)):
+            total += num
+        else:
+            print("NaN")
+            return "NaN"
+    
+    print(total)
+    return total
+
+add_list(1,50,1.23)
 #-----------------------------------------------
-
-
-
 
 # Challenge 2: remove_ends
 
@@ -31,6 +40,16 @@
 
 #-----------------------------------------------
 # Solution Goes Here - >
+def remove_ends(string):
+    if len(string) > 3:
+        new_string = string[1:-1]
+        print(new_string)
+        return new_string
+    else: 
+        print('')
+        return ''
+
+remove_ends("Austin")
 #-----------------------------------------------
 
 
@@ -50,6 +69,17 @@
 
 #-----------------------------------------------
 # Solution Goes Here - >
+def is_palindrome(string):
+    string = string.strip().replace(" ","").lower()
+    reversed = string[::-1]
+    if(string == reversed):
+        print(True)
+        return True
+    else:
+        print(False)
+        return False
+
+is_palindrome('A nut for a jar of tuna')
 #-----------------------------------------------
 
 
@@ -68,6 +98,18 @@
 
 #-----------------------------------------------
 # Solution goes here ->
+def is_prime(number):
+    if number > 1:
+        for i in range(2, int(number/2)+1):
+            if (number % i) == 0:
+                print(number, "is not a prime number")
+                break
+        else:
+            print(number, "is a prime number")
+    else:
+        print(number, "is not a prime number")
+
+is_prime(200)
 #-----------------------------------------------
 
 
@@ -79,17 +121,31 @@
 
 # Your function should take the list and the user's homestate as arguments
 
-# shopping_cart = [ 
-#   {"item": "headphones", "price": 25},
-#   {"item": "speakers", "price": 40 },
-#   {"item": "microphone", "price": 70},
-#   {"item": "lamp", "price": 15 },
-#   {"item": "tower fan", "price": 35 },
-# ]
+shopping_cart = [ 
+  {"item": "headphones", "price": 25},
+  {"item": "speakers", "price": 40 },
+  {"item": "microphone", "price": 70},
+  {"item": "lamp", "price": 15 },
+  {"item": "tower fan", "price": 35 },
+]
 
 
 #-----------------------------------------------
 # Solution Goes Here ->
+def total_checkout_cost(list, homestate):
+    total_cost = 0
+    ten_shipping_states = set(('HI', 'AK', 'TX','FL'))
+    five_shipping_states = set(('AL', 'MS', 'NV','IL'))
+    for num in list:
+        total_cost += round((num["price"]*1.085),2)
+    if homestate in ten_shipping_states:
+        total_cost += 10
+    elif homestate in five_shipping_states:
+        total_cost += 5
+    else: total_cost +=0
+    print(total_cost)
+
+# total_checkout_cost(shopping_cart,'TX')
 #-----------------------------------------------
 
 
@@ -107,6 +163,22 @@
 
 #-----------------------------------------------
 # Solution Goes Here ->
+
+def fizz_buzz():
+    for number in range(1,50):
+        if isinstance(number, (int, float)): 
+            if (number % 3 == 0 and number % 5 == 0):
+                print(f' {number} FizzBuzz')
+            elif number % 3 == 0:
+                print(f' {number} Fizz')
+            elif number % 5 == 0:
+                print(f' {number} Buzz')
+            else:
+                print(number)
+        else: print(f'"{number}" is not a number')
+    
+
+# fizz_buzz()
 #-----------------------------------------------
 
 
@@ -145,5 +217,31 @@
 
 
 #-----------------------------------------------
-# Solution Goes Here - >
+# Solution Goes Here - > 
+# def chess_board(rows, columns):
+#     board = []
+#     for r in range(rows):
+#         row = []
+#     for c in range(columns):
+#         if(r+c) % 2 == 0:
+#             row.append('O')
+#         else: row.append('X')
+#     board.append(row)
+#     print(chess_board(6,4))
+
+def chess_board(rows, columns):
+    board = []
+    for r in range(rows):
+        row = []
+        for c in range(columns):
+            if (r+c) % 2 == 0:
+                row.append('O')
+            else:
+                row.append('X')
+        board.append(row)
+    return board
+
+# print(chess_board(3,7))
+
+    
 #-----------------------------------------------
